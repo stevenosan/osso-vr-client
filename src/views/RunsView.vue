dcfe6db3dacd80db351bdf741ac75f8e436df645
Steve <stevenosan@gmail.com>
2024-11-10 20:23:27 -0600

    cleaning up the RunsView

--- src/views/RunsView.vue
+++ src/views/RunsView.vue
@@ -11,6 +11,27 @@ onMounted(() => {
 
 <template>
   <div class="runs">
+    <v-list lines="four" class="scrollable-list">
+      <v-list-item-group v-if="runsStore.sortedRuns">
+        <v-list-item v-for="run in runsStore.runs" :key="run.id" :to="'run/' + run.id">
+          <v-list-item-content>
+            <v-list-item-title class="headline">{{
+              new Date(run.date).toLocaleDateString()
+            }}</v-list-item-title>
+            <v-list-item-subtitle class="body-2">
+              <div><strong>User:</strong> {{ run.userName }}</div>
+              <div><strong>Completed:</strong> {{ run.completed }}</div>
+              <div><strong>Passed:</strong> {{ run.passed }}</div>
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </div>
+</template>
+
+<!-- <template>
+  <div class="runs">
     <v-list lines="two" class="scrollable-list">
       <v-list-item
         v-for="run in runsStore.sortedRuns"
@@ -22,7 +43,7 @@ onMounted(() => {
       </v-list-item>
     </v-list>
   </div>
-</template>
+</template> -->
 
 <style scoped>
 .scrollable-list {
